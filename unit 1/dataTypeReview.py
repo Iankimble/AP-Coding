1035c2fa127566de3aa8be08f9e0f674eab6032f
Ian <38480036+Iankimble@users.noreply.github.com>
2025-08-22 15:17:37 +0000

    foundation review

---
+++ unit 1/dataTypeReview.py
@@ -0,0 +1,31 @@
+print('Ian Kimble')
+
+# W3Schools.com - our friend
+
+# 1.
+# print each of the following data types:
+
+# string
+print("hello world")
+
+# boolean
+print(2 > 1)
+print(True)
+b = 3
+a = 2
+print(b > a)
+
+# integer
+print(1)
+print(1234)
+z = 32903902
+print(z)
+
+# float
+print(10.00)
+print(121.1212)
+
+# 2.
+# create and print a list that features 2 of each data type. 
+food = ["potato","salad","candy"]
+print([True, False, 10, 100, 10.5, 10.3, "I like apples", "4" ])
