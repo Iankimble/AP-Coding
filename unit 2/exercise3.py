77a10af16106a4faefebe44e0a8cb80bd3ac5324
Ian <38480036+Iankimble@users.noreply.github.com>
2025-10-22 14:30:41 +0000

    exercise 3

---
+++ unit 2/exercise3.py
@@ -0,0 +1,12 @@
+# Copy the new weeklyPlayerStats() function into your helperFunction.py file.
+
+# Create a new python document caled  exercise3.py . Using th individual player helper function, 
+# answer the following questions:
+ 
+# 1. Who are the top 5 quarterbacks by passing yards? What’s their average completion percentage (cmp_pct)?
+
+# 2. What might a high cmp_pct tell you about a quarterback’s style of play?
+
+# 3. Which RB had the highest rushing yards that week? Who had the best yards per carry (rush_ypc)?
+
+# 4. If a RB has high rush_yards but low rush_ypc, what could that mean?
