3be942c6028a4f9217c9ba06752fcfb413450c21
Ian <38480036+Iankimble@users.noreply.github.com>
2025-10-22 11:18:45 +0000

    individual playr hlpr function

--- unit 2/exercise1.py
+++ unit 2/exercise1.py
@@ -1,29 +1,97 @@
 import pandas as pd
 import nfl_data_py as nfl
 
-# Load 2024 NFL schedule data
-schedules = nfl.import_schedules([2024]) # schedule data
-weeklyStats = nfl.import_weekly_data([2024]) # weekly game data
-pbp = nfl.import_pbp_data([2024]) # game play by play data
+from helperFunctions import get_team_records, get_season_Results_By_team
 
-# -- Shows all columns on object --
-# print(pbp.columns.tolist()) 
-# print(weeklyStats.columns) 
-# print(schedules.columns)
+# schedules = nfl.import_schedules([2024])
+ 
+# print(schedules.columns.tolist())
 
-team_schedule = schedules[
-    (schedules["home_team"] == 'PHI') | (schedules["away_team"] == 'PHI')
-]
+records = get_team_records(2025)
 
-# print(team_schedule)
 
-schedules[["week", "home_team", "away_team", "home_score", "away_score"]] # query only select few columns
+birds = get_season_Results_By_team(2025,'NYJ')
+print(birds)
+pd =[ 4, 3, 7, 6, -4,  -17]
 
-# print(regular_season)
-
-# quick notes
-# querying columns can have different column names do to WHEN it was uploaded - older data my have a different column name
-# it can also be a result of WHERE it was queried from nflR
+# COLTS AND BUCS FOR HIGHEST PD
+# COLTS 78
+# TAMPA 14
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# 'What is the highest point differential for the eagles ?'
+# 'What is the highest point differential in the league this season? '
+# 'What is the lowest point differential in the league this season? '
+
+# 'Point Differential=Points For Points Against'
+
+# 'Points For (PF) = total points the team has scored.'
+
+# 'Points Against (PA) = total points the team has allowed'
+
+
+
+
+
+
+
+#  print(records[['team','points_for', 'points_against']])
+
+# print(records[['wins']].mean()) # what was the average wins for the years
+
+# Find a season where the average wins was below 8.0 in the last 10 years ? 
+
+# 2022 - 8.4 wins 
+# 2020 - 7.9 wins 
+
+# 1. Find at least 2 teams that have average of at least 12 wins over 
+# the past 5 seasons
+
+# Cheif and Bills
+
+
+
+
+
+
+
+
+
+
+
+
+
+# 2. What is the median wins for teams over the past 5 seasons.
+
+# - points for = how many points a team scored on opponents
+# - points allowed = how may points a team allowed by opponents
+
+# 3. What team had the highest points scored this past season- whwat was there record
+# print(records[['team','points_for']].max()) # notation for getting specfic columns
+
+# 4. What team had the least amount scored this past season - what was there record
+# print(records[['team','points_for']].min()) # notation for getting specfic columns
+
+# which team has the highest and lowest point differential? What were their records?
+
+# Do you think a team can score a lot of points but still lose games? Why?
+
+# Do teams with more wins always have more points_for?
+
+# what is the average points for scored in the nfl and how many teams are above the average
---
+++ unit 2/exercise2.py
@@ -0,0 +1,85 @@
+import pandas as pd
+import nfl_data_py as nfl
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+from helperFunctions import get_team_records, get_season_Results_By_team, weeklyPlayerStats
+
+# schedules = get_team_records(2025)
+# print(schedules)
+
+# My hypothesis - The best point differentials are probably
+# going to be the best teams.
+
+top6_Teams = ['TB','IND','LA','BUF','SF','SEA']
+
+# find the point differential for each team listed
+# formula for PD = points for - points against
+
+team_1 = get_season_Results_By_team(2025,'TB') # 14 PD
+team_2= get_season_Results_By_team(2025,'IND') # 78 PD
+team_3= get_season_Results_By_team(2025,'LA')
+team_4= get_season_Results_By_team(2025,'BUF') # 7~ PD
+team_5= get_season_Results_By_team(2025, 'SF') # -8 PD
+team_6= get_season_Results_By_team(2025, 'SEA')
+
+#print(team_1)
+# print(team_2)
+# print(team_3)
+# print(team_4)
+# print(team_5)
+print(team_6)
+
+# 1. Which team has the best point differential this season ?
+'The best overall PD is INDY. They have a point differiential of 78 pts'
+
+# 2. Which team has the worst point differential this season ?
+'The worst overall PD is SF. They have a point differiential of -3 pts.'
+
+# 3. Which team has the best home point differential this season ?
+'The best home PD is...'
+
+# 4. Which team has the best away point differential this season ? 
+'The best away PD is...'
+
+
+# In your exercise2.py document, create a function that will 
+# allow you to enter different numbers individually and 
+# calculate the sum of those numbers. Your function should operate 
+# on a while loop and allow the user to enter each number 
+# 1 at a time, as many times as they want. Your function 
+# should ask the user if they are done entering numbers,  
+# When the user is done entering numbers your function 
+# should add up and return the sum of those numbers. 
+# If they are not done entering in numbers it should allow 
+# them to enter another one.
+
+
+
+
+
+def pdCheck():
+    print("Please enter a number")
+    number = input()
+    values = []
+    
+    while number != 'q':
+        values.append(int(number))
+        print(values)
+        print("Please enter a number")
+        number = input()          
+    else:
+        print('doing caluclation...') 
+        total = sum(values)
+        print(total)
+
+# 3. Which team has the best home point differential this season ?
+'The best home PD is...INDY 64' 
+
+# 4. Which team has the best away point differential this season ? 
+'The best away PD is...SEA 19'
+ 
+pdCheck()
+
+
