4edee284b5262b37c7d91fdc06cd07378e031621
Ian <38480036+Iankimble@users.noreply.github.com>
2025-10-29 14:38:51 +0000

    exercise 3

--- unit 2/exercise3.py
+++ unit 2/exercise3.py
@@ -1,17 +1,30 @@
+from helperFunctions import weeklyPlayerStats
+
 # Copy the new weeklyPlayerStats() function into your helperFunction.py file.
 
 # Create a new python document caled  exercise3.py . Using th individual player helper function, 
 # answer the following questions:
 
 # NOTE = you will need to pass the position, year and week in as an agrument
-# position example : quarterback = 'QB', runnerback = 'RB'
+# position example:
+  
+# - quarterback = 'QB'
+# - runnerback = 'RB'
+# - wide receiver = 'WR'
 
-#weeklyPlayerStats('QB',2025,1) 
+exampleData = weeklyPlayerStats(2024,'QB', 1) 
+print(exampleData)
+# NOTE = Please write your responses in string format 
+'Please write your responses in string format'
 
-# 1. Who are the top 5 quarterbacks by passing yards? What’s their average completion percentage (cmp_pct)?
+# 1. Who are the top 5 quarterbacks by passing yards in 2024? 
+# What was their average completion percentage (cmp_pct)?
 
-# 2. What might a high cmp_pct tell you about a quarterback’s style of play?
+# 2. What does a high cmp_pct (completion percentage) tell you about a
+# quarterback’s style of play?
 
-# 3. Which RB had the highest rushing yards that week? Who had the best yards per carry (rush_ypc)?
+# 3. Which RB had the highest rushing yards in 2024? 
+# Which RB had the best yards per carry (rush_ypc) in 2024? Are these the same or different individuals?
 
-# 4. If a RB has high rush_yards but low rush_ypc, what could that mean?
+# 4. If a RB has high rushing yard (rush_yards), but low rushing yards per carry (rush_ypc),
+#  what could that mean?
